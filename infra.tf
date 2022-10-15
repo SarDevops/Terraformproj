293ec35a5c566b35882157721d50bb518677803f
SarDevops <sardevops1@gmail.com>
2022-10-15 19:02:57 +0530

    Added second subnet

--- infra.tf
+++ infra.tf
@@ -17,7 +17,7 @@ resource "azurerm_subnet" "example" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_subnet" "example" {
+resource "azurerm_subnet" "example1" {
   name                 = "Testing-subnet-2"
   resource_group_name  = azurerm_resource_group.myinfra.name
   virtual_network_name = azurerm_virtual_network.example.name
